361bca66c5066ee322cf78a822afd2292b225f9d
Peter Wu <petey888@gmail.com>
2019-08-02 10:08:34 -0700

    Added documentation for: - MeasurementSystem enum - lbToKg and inchToMeter double extensions

--- BMI Calculator/Utility.swift
+++ BMI Calculator/Utility.swift
@@ -8,6 +8,11 @@
 
 import Foundation
 
+
+/// Enum for categorizing two types of input.  An input is either metric or imperial system.
+///
+/// - metric: represents metric system input
+/// - imperial: represents imperial system input
 enum MeasurementSystem: Int, Codable {
     
     case metric
@@ -16,16 +21,15 @@ enum MeasurementSystem: Int, Codable {
 
 extension Double {
     
+    /// calculates kilogram equivalent of pound
     var lbToKg: Double {
         return self * 0.453592
     }
     
+    /// Calculates meters equivalent of inches
     var inchToMeter: Double {
         return self * 0.0254
     }
     
-    var inchToCentimeter: Double {
-        return self.inchToMeter / 100
-    }
 }
 
